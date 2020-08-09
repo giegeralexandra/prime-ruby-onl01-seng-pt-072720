b0573d9686e3663ba48a92b96cc5f8dfaf8cafeb
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-09 16:52:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(integer)
+  n = 2 
+  if integer <= 1 
+    return false 
+  else
+     while n < 7  
+        if integer % n == 0 
+          return false
+        n += 1 
+          else
+            return true 
+         end
+        end
+  end
+end 
