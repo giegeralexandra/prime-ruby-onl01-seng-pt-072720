d168ed8762856e8b0cbf9c96a4c35f329fa4c5b5
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-09 17:18:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,10 +6,10 @@ def prime?(integer)
      while n < integer  
         if integer % n == 0 
           return false
-        n += 1 
+          n += 1 
           else
             return true 
          end
-        end
+      end
   end
 end 
