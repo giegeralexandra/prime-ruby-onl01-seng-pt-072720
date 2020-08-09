98dd171d8639b5d5f65cfb79fafbc175a7f875d1
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-09 17:46:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,14 @@
 def prime?(integer)
   n = 2 
   if integer <= 1 
-      return false 
-    elsif integer % [2..integer-1] == 0 
+    return false 
+  end
+  while n < integer 
+    if integer % n == 0 
       return false
+      n += 1 
     else 
       return true 
-  end
-end  
-     
-  
+    end
+   end
+end 
