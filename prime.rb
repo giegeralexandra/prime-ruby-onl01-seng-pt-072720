7f84d1d8b38339624af85d2f55af84653302a0f2
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-09 17:54:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(integer)
+  n = 2 
+  if integer <= 1 
+    return false 
+  elsif integer == 2 
+    return true 
+  else 
+    while n < integer 
+      if integer % n == 0 
+        return false
+      end
+      n+=1
+    end
+  end
+  return true 
+end 
